b36670be48b10792a5db036dbc103414596190a8
Teerasej <teerasej@gmail.com>
2024-09-19 11:03:55 +0700

    create loading logic

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,3 +1,11 @@
 import 'package:get/get.dart';
 
-class ProfileController extends GetxController {}
+class ProfileController extends GetxController {
+  var loading = false.obs;
+
+  void loadDataFromWebAPI() {
+    loading.value = true;
+
+    loading.value = false;
+  }
+}
