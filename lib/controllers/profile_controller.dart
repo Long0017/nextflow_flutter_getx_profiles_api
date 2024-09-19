4571e6cf125b5639c4102dd1ca0126ecd08a9c5b
Teerasej <teerasej@gmail.com>
2024-09-19 11:27:30 +0700

    add delay to loading method

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -3,8 +3,10 @@ import 'package:get/get.dart';
 class ProfileController extends GetxController {
   var loading = false.obs;
 
-  void loadDataFromWebAPI() {
+  void loadDataFromWebAPI() async {
     loading.value = true;
+
+    await Future.delayed(Duration(seconds: 3));
 
     loading.value = false;
   }
